4e9e6c3576a18dcec67e70eb457cdc74825c21e0
sesh <122727594+exSesh@users.noreply.github.com>
2023-01-17 01:33:30 +0300

    Update script_webinar.php

--- script_webinar.php
+++ script_webinar.php
@@ -15,7 +15,7 @@ if($conn->connect_error){
 $sql = "SELECT name, family FROM webinar JOIN webinarx ON webinar.id = webinarx.id WHERE webinar.id = FLOOR(RAND()*(10-1)*1);";
 if($result = mysqli_query($conn, $sql)){
     while($row = mysqli_fetch_array($result)){
-echo $row["name"] . "" . $row["family"] . " from " . gethostname();
+echo $row["name"] . $peremen . $row["family"] . $peremen . " from " . $peremen . gethostname();
 	}
 }
 $conn->close();
