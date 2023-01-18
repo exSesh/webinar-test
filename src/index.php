9f018f2d87bf1081c28c7c5e64afb4239665f0e3
sesh <122727594+exSesh@users.noreply.github.com>
2023-01-18 16:35:06 +0000

    modified:   src/index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@ if($conn->connect_error){
 $sql = "SELECT name, family FROM webinar JOIN webinarx ON webinar.id = webinarx.id WHERE webinar.id = FLOOR(RAND()*(10-1)*1);";
 if($result = mysqli_query($conn, $sql)){
     while($row = mysqli_fetch_array($result)){
-echo $row["name"] . $peremen . $row["family"] . $peremen . "from" . $peremen . gethostbyaddr($_SERVER['172.17.0.1:3306']);
+echo $row["name"] . $peremen . $row["family"] . $peremen . "from" . $peremen . gethostbyaddr('172.17.0.1:3306');
 	}
 }
 $conn->close();
