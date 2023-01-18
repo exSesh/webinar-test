afe3ff5d4228ed3cea0693b4a7c867112e65269a
sesh <122727594+exSesh@users.noreply.github.com>
2023-01-18 17:41:10 +0000

    modified:   src/index.php 	modified:   src/script_webinar.php

--- src/script_webinar.php
+++ src/script_webinar.php
@@ -9,17 +9,17 @@
 
 <?php
 $peremen = " ";
-$conn = new mysqli("127.0.0.1", "root", "8hXI5MgIbqifZ", "webinar_bd");
-if($conn->connect_error){
-    die("Ошибка: " . $conn->connect_error);
-}
+for ($i = 3306; $i <= 3310; $i++){
+$conn = new mysqli("172.17.0.1", "root", "8hXI5MgIbqifZ", "webinar_bd", $i);
 $sql = "SELECT name, family FROM webinar JOIN webinarx ON webinar.id = webinarx.id WHERE webinar.id = FLOOR(RAND()*(10-1)*1);";
 if($result = mysqli_query($conn, $sql)){
     while($row = mysqli_fetch_array($result)){
-echo $row["name"] . $peremen . $row["family"] . $peremen . "from" . $peremen . gethostname();
+echo $row["name"] . $peremen . $row["family"] . $peremen . "from" . $peremen . gethostbyaddr('172.17.0.1:$i');
 	}
 }
 $conn->close();
+}
+
 ?>
 </body>
 </html>
