96c0254f20246ac569c6c56924144184b76cd913
sesh <122727594+exSesh@users.noreply.github.com>
2023-01-18 17:30:22 +0000

    modified:   src/index.php

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@ $peremen = " ";
 for ($i = 3306; $i <= 3310; $i++){
 $conn = new mysqli("172.17.0.1", "root", "8hXI5MgIbqifZ", "webinar_bd", $i);
 if($conn->connect_error){
-    die("Ошибка: " . $conn->connect_error);
+    continue("Ошибка: " . $conn->connect_error);
 }
 $sql = "SELECT name, family FROM webinar JOIN webinarx ON webinar.id = webinarx.id WHERE webinar.id = FLOOR(RAND()*(10-1)*1);";
 if($result = mysqli_query($conn, $sql)){
