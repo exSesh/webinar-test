6a67e6c4a95a90911dc9f40e324b6537e73a6994
sesh <122727594+exSesh@users.noreply.github.com>
2023-01-18 17:28:51 +0000

    modified:   src/index.php

--- src/index.php
+++ src/index.php
@@ -11,6 +11,9 @@
 $peremen = " ";
 for ($i = 3306; $i <= 3310; $i++){
 $conn = new mysqli("172.17.0.1", "root", "8hXI5MgIbqifZ", "webinar_bd", $i);
+if($conn->connect_error){
+    die("Ошибка: " . $conn->connect_error);
+}
 $sql = "SELECT name, family FROM webinar JOIN webinarx ON webinar.id = webinarx.id WHERE webinar.id = FLOOR(RAND()*(10-1)*1);";
 if($result = mysqli_query($conn, $sql)){
     while($row = mysqli_fetch_array($result)){
@@ -19,6 +22,7 @@ echo $row["name"] . $peremen . $row["family"] . $peremen . "from" . $peremen . g
 }
 $conn->close();
 }
+
 ?>
 </body>
 </html>
